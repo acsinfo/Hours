01dfda4632561df5dede418319a83c711f849deb
Sigfrid Dusci <sig@kyooem.com>
2016-10-05 17:01:55 +0800

    scope reports

--- spec/features/user_views_personal_report_spec.rb
+++ spec/features/user_views_personal_report_spec.rb
@@ -46,11 +46,3 @@ feature "User views personal report" do
     end
   end
 end
-
-def add(users, subdomain)
-  Apartment::Tenant.switch(subdomain)
-  Array(users).each do |user|
-    user.save
-  end
-  Apartment::Tenant.reset
-end
